69506d55bbb366a4672ce1d6f83e4154a7076b5f
pikachuzzZ <863484799@qq.com>
2022-01-27 15:16:13 +0800

    first commit

---
+++ src/main/java/org/example/test.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class test {
+    public static void main(String[] args) {
+//         int a = 1;
+//         int b = 0;
+//         if(b > 0 && a++ >0){
+//
+//         }
+//
+//         System.out.println(b);
+//         System.out.println(a);
+//         System.out.println("----------------");
+//         if(b>0 & a++ >0){
+//
+//         }
+//         System.out.println(b);
+//         System.out.println(a);
+//
+         boolean b = true;
+         if (b = false){
+              System.out.println("a");
+         }else if(!b){
+              System.out.println("b");
+         }
+    }
+}
