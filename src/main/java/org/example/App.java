00b3973f3efc75c874b96063f1403f19a58c30a5
pikachuzzZ <863484799@qq.com>
2022-01-27 15:42:50 +0800

    hot-fix second commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello git v1!" );
         System.out.println( "Hello git v2!" );
         System.out.println( "Hello git hot-fix!" );
+        System.out.println( "Hello git conflict hot-fix!" );
     }
 }
